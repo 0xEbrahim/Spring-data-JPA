118224f50b1a461f64705bd87594f2c0fcbaab09
0xEbrahim <elsayedebrahim603@gmail.com>
2025-09-06 20:14:55 +0300

    adding author entity

---
+++ src/main/java/com/ibrahim/Jpa/models/Author.java
@@ -0,0 +1,43 @@
+package com.ibrahim.Jpa.models;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+@Entity(name = "authors")
+public class Author {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_seq")
+    @SequenceGenerator(name = "author_seq", sequenceName = "author_seq", allocationSize = 4)
+    private Integer id;
+
+    @Column(length = 35)
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private Integer age;
+
+
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+
+
+    @Column(
+            insertable = false,
+            nullable = true
+    )
+    private LocalDateTime  updatedAt;
+}
