05f801dead1c39e4fcdd96dbf1711cdf73b5344e
0xEbrahim <elsayedebrahim603@gmail.com>
2025-09-06 20:45:11 +0300

    add course entity

---
+++ src/main/java/com/ibrahim/Jpa/models/Course.java
@@ -0,0 +1,23 @@
+package com.ibrahim.Jpa.models;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_seq")
+    @SequenceGenerator(name = "author_seq", sequenceName = "author_seq", allocationSize = 4)
+    private Integer id;
+
+    private String name;
+
+    private String description;
+}
